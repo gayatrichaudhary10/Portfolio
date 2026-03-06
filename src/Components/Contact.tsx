d235c84b42b52aa64d5757cb8b9572efaf609056
gayatrichaudhary10 <gayatric1012@gmail.com>
2026-03-06 13:37:29 +0530

    fix vercel build error

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { motion,  } from "framer-motion";
 import { FaEnvelope, FaLinkedin, FaPhone } from "react-icons/fa";
 const Contact: React.FC = () => {
